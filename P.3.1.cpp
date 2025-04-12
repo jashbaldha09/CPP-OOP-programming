be97a610ca0869355054ec355f807654e1f221a2
jashbaldha09 <jashbaldha09@gmail.com>
2025-04-13 01:25:50 +0530

    P.3.1.cpp

--- P.3.1.cpp
+++ P.3.1.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class PayRoll
 {
 private:
     string EmployeeName;
-    int EmployeeSalary,EmployeeBonus,EmployeeID;
+    int EmployeeSalary, EmployeeBonus, EmployeeID;
 
 public:
 
-    PayRoll(){}
+    PayRoll() {}
 
-    PayRoll(string MainName,int MainSalary,int MainID,int MainBonus = 500)
+    PayRoll(string MainName, int MainSalary, int MainID, int MainBonus = 500)
     {
         EmployeeName = MainName;
         EmployeeSalary = MainSalary;
@@ -26,44 +27,46 @@ public:
 
     void DisplayInfo()
     {
-        cout<<"Name   : "<<EmployeeName<<endl;
-        cout<<"ID     : "<<EmployeeID<<endl;
-        cout<<"Total  : "<<EmployeeSalary<<endl;
+        cout << "Name   : " << EmployeeName << endl;
+        cout << "ID     : " << EmployeeID << endl;
+        cout << "Total  : " << EmployeeSalary+EmployeeBonus << endl;
     }
 };
 
 int main()
 {
     string MainName;
-    int MainSalary,MainID,MainBonus;
-    int n;
-    cout<<"Enter the numbers of the Employee : ";
-    cin>>n;
-    cout<<endl;
-    PayRoll employee[n];
+    int MainSalary, MainID, MainBonus;
 
-    for(int i=0 ; i<n ; i++)
+    vector<PayRoll> employeelist;
+
+    cout<<"Enter 'stop' to exit"<<endl;
+    while (true)
     {
-        cout<<"Enter your Name : ";
-        cin>>MainName;
+        cout << "Enter your Name : ";
+        cin >> MainName;
+        if (MainName == "stop")
+            break;
 
-        cout<<"Enter your ID : ";
-        cin>>MainID;
+        cout << "Enter your ID : ";
+        cin >> MainID;
 
-        cout<<"Enter your Salary : ";
-        cin>>MainSalary;
+        cout << "Enter your Salary : ";
+        cin >> MainSalary;
 
-        cout<<"Enter your BonusSalary : ";
-        cin>>MainBonus;
-        cout<<endl;
-        employee[i] = PayRoll(MainName,MainSalary,MainID,MainBonus);
+        cout << "Enter your BonusSalary : ";
+        cin >> MainBonus;
+        cout << endl;
+
+        PayRoll employee(MainName, MainSalary, MainID, MainBonus);
+        employeelist.push_back(employee);
     }
 
-    for(int i=0 ; i<n ; i++)
+    for (int i = 0; i < employeelist.size(); i++)
     {
-        cout<<endl<<"Information of Employee "<<i+1<<endl;
-        employee[i].DisplayInfo();
+        cout << endl << "Information of Employee " << i + 1 << endl;
+        employeelist[i].DisplayInfo();
     }
 
-    cout<<endl<<"Created by Jashkumar : 24CE004"<<endl;
+    cout << endl << "Created by Jashkumar : 24CE004" << endl;
 }
